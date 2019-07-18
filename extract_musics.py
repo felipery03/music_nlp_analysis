b3ff2582c2f06ff86bcf818b92d6d8928f7a73c0
pkeiko <paula.keiko.m@gmail.com>
2019-07-18 00:01:44 -0300

    funlçao que estuda a frase e traduz o que não está em português

--- extract_musics.py
+++ extract_musics.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from textblob import TextBlob as tb #pip install textblob
+import nltk
+
+
+
 
 url = 'https://www.vagalume.com.br/'
 
@@ -40,25 +45,76 @@ def get_letras_top20_musics(artista):
     testes = htmlparse.find('div', {'class':'artista-top g-sp g-pr'})
     music = testes.findAll('span')
     return music
-    
+
+# translate phrase words to pt 
+def get_pt(frase, vizinhanca):
+    fraseretorno = ""
+    split_frase = frase.split(" ")
+    for x in range (0, len(split_frase)):
+        palavra = split_frase[x]
+        if(len(palavra)>2):                 #function only works for words with len>2
+            if(check_isLanguage(palavra)):      #checks if word and phrase are same language
+                fraseretorno+=palavra
+            else:                           #not same language
+                fraseretorno+=getFinalword(split_frase,x,vizinhanca)
+        else:
+            fraseretorno+=palavra
+        fraseretorno+=" "
+    return fraseretorno
+
+def check_isLanguage(palavra):
+    palavratb = tb(palavra)
+    lang_palavra = palavratb.detect_language()
+    if(lang_palavra != "pt"):
+        return False
+    return True
+
+def getFinalword(vetor_frase, palavra_position, neighborhood_size):       #check with neighborhood and gives final word
+    menor, maior = calculo_neighborhood(vetor_frase,palavra_position, neighborhood_size)
+    frasefinal = ""
+    for i in range (menor, maior+1):
+        frasefinal+= " " + vetor_frase[palavra_position+i]            
+    frasefinaltb = tb(frasefinal)
+    lang_frasefinal = frasefinaltb.detect_language()
+    if(lang_frasefinal != "pt"):
+        palavratb = tb(vetor_frase[palavra_position])
+        return str(palavratb.translate(to="pt"))
+    return vetor_frase[palavra_position]    
+
+def calculo_neighborhood(vetor_frase, palavra_position,neighborhood_size):
+    menor = 0
+    for i in range (neighborhood_size,0,-1):                    
+        if palavra_position-i >= 0:
+            menor = -i                        
+            break
+    maior = 0
+    for i in range (neighborhood_size,0,-1):
+        if palavra_position+i < len(vetor_frase):
+            maior = i
+            break
+    return menor, maior
+            
+
 
 # Testing funtctions above:
 
 
-top_100 = get_top_100()
+# top_100 = get_top_100()
 
-final_list = []
+# final_list = []
 
-for artist in top_100:
-    music_names = get_music_names(artist)
-    for music in music_names:
-        lyrics = get_lyrics(url + music)
-        final_list.append([artist, music, lyrics])
+# for artist in top_100:
+#    music_names = get_music_names(artist)
+#    for music in music_names:
+#        lyrics = get_lyrics(url + music)
+#        final_list.append([artist, music, lyrics])
 
-df = pd.DataFrame(final_list, columns=['artist', 'music_name', 'lyrics'])
-df.to_csv('data/dataset_lyrics.csv', index=False) 
+# df = pd.DataFrame(final_list, columns=['artist', 'music_name', 'lyrics'])
+# df.to_csv('data/dataset_lyrics.csv', index=False) 
 
-top20music = get_letras_top20_musics("lady gaga")
-for m in top20music:
-    print(m.text)
+# top20music = get_letras_top20_musics("lady gaga")
+# for m in top20music:
+#    print(m.text)
 
+print(get_pt("eu perguntava do you wanna dance?",1))
+print(get_pt("juntos e shallow now",1))
